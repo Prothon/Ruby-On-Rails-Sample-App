ae5f885606faaa93049c0a5a0e0c8436f7f45e8a
Andrew Raymer <Andrew_Raymer@opticalonline.com>
2014-06-01 15:30:46 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '390d3e365752c8e3b16c004359a8131e7b328a5a570e00fad4f4a9742b7a0c7b7059f494f41f55036c8695d6c3cfe2f81990a370afcd847e58501368e780f168'
